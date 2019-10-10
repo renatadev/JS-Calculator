ae1ea4ef20b6d4a527719d3e2cbfec3db0a43037
Renata <[renatajarmova@gmail.com]>
2019-10-10 12:54:57 +0100

    Access all elem from the DOM

---
+++ js/script.js
@@ -0,0 +1,9 @@
+//Access all elements from the DOM
+
+const numberButtons = document.querySelectorAll('[data-number]');
+const operationButtons = document.querySelectorAll('[data-operation]');
+const equalsButton = document.querySelector('[data-equals]');
+const deleteButton = document.querySelector('[data-delete]');
+const allClearButton = document.querySelector('[data-all-clear]');
+const previousOperandText = document.querySelector('[data-previous-operand]');
+const currentOperandText = document.querySelector('[data-current-operand]');
