70ae049f94e62cb90c247534285dc57fd3d96022
Renata <[renatajarmova@gmail.com]>
2019-10-11 12:45:04 +0100

    Create calculator class and its empty functions

--- js/script.js
+++ js/script.js
@@ -7,3 +7,41 @@ const deleteButton = document.querySelector('[data-delete]');
 const allClearButton = document.querySelector('[data-all-clear]');
 const previousOperandText = document.querySelector('[data-previous-operand]');
 const currentOperandText = document.querySelector('[data-current-operand]');
+
+
+// Calculator class - containing all the inputs and functions for the calculator
+//Display of outputs
+
+class Calculator {
+  //where to place the display for the calculator
+  constructor(previousOperandText, currentOperandText) {
+    this.previousOperandText = previousOperandText;
+    this.currentOperandText = currentOperandText;
+  }
+
+  //operations that the calculator class can perform - functions
+  clear() {
+
+  }
+
+  delete() {
+
+  }
+
+  appendNumber(number) {
+
+  }
+
+  chooseOperation(operation) {
+
+  }
+
+  compute() {
+
+  }
+
+  updateDisplay() {
+    
+  }
+
+}
